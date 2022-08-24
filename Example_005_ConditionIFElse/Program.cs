d987b4039c63424a3d0c4d858cee1033437fdd73
Elena <am1ks@yandex.ru>
2022-08-24 11:32:36 +0300

    Example_005 ConditionIfElse

---
+++ Example_005_ConditionIFElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "ахмед")
+{
+    Console.Write("Эээээээ");
+}
+else
+{
+    Console.Write("Здорово! ");
+    Console.WriteLine(username);
+}
