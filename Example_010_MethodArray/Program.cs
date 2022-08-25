c196569dc39a6f1ce0ffa43e0e7560a64e0e91fa
Elena <am1ks@yandex.ru>
2022-08-25 13:32:39 +0300

    Method

---
+++ Example_010_MethodArray/Program.cs
@@ -0,0 +1,16 @@
+﻿int [] array = {13,24,36,49,50,93,72,81,93};
+
+int n = array.Length;
+int find = 93;
+
+int index = 0;
+
+while (index < n)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+}
