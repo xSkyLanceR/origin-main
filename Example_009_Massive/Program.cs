2176e2f3601049be0f5993318425c6a61ec72665
Elena <am1ks@yandex.ru>
2022-08-25 13:23:54 +0300

    Massive2

---
+++ Example_009_Massive/Program.cs
@@ -0,0 +1,14 @@
+﻿int Max (int arg1, int arg2, int arg3)
+{
+    int result = arg1;
+    if (arg2 > result) result = arg2;
+    if (arg3 > result) result = arg3;
+    return result;
+}
+
+int [] array = {13,24,36,49,50,63,72,81,93};
+
+
+int max = Max(Max(array[0], array[1], array[2]), Max(array[3], array[4], array[5]), Max(array[6], array[7], array[8]));
+
+Console.WriteLine(max);
